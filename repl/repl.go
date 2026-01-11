3c579f4dd9dad896196574474831218302952a15
yuys13 <yuys13@users.noreply.github.com>
2026-01-11 16:12:14 +0900

    feat(chapter3.5.2): evaluate integer literals and update REPL

--- repl/repl.go
+++ repl/repl.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 
+	"github.com/yuys13/monkey/evaluator"
 	"github.com/yuys13/monkey/lexer"
 	"github.com/yuys13/monkey/parser"
 )
@@ -31,8 +32,14 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+		// io.WriteString(out, program.String())
+		// io.WriteString(out, "\n")
+
+		evaluated := evaluator.Eval(program)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
 	}
 }
 
