9fa606f6a48e987fa57bdef9463146926b120d75
yuys13 <yuys13@users.noreply.github.com>
2026-01-11 23:21:58 +0900

    feat(chapter3.9): add bindings and environment

--- repl/repl.go
+++ repl/repl.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/yuys13/monkey/evaluator"
 	"github.com/yuys13/monkey/lexer"
+	"github.com/yuys13/monkey/object"
 	"github.com/yuys13/monkey/parser"
 )
 
@@ -14,6 +15,7 @@ const PROMPT = ">> "
 
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
+	env := object.NewEnvironment()
 
 	for {
 		fmt.Printf(PROMPT)
@@ -35,7 +37,7 @@ func Start(in io.Reader, out io.Writer) {
 		// io.WriteString(out, program.String())
 		// io.WriteString(out, "\n")
 
-		evaluated := evaluator.Eval(program)
+		evaluated := evaluator.Eval(program, env)
 		if evaluated != nil {
 			io.WriteString(out, evaluated.Inspect())
 			io.WriteString(out, "\n")
