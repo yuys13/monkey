393e4871ff201528baed0d70fd4c4df017f56415
yuys13 <yuys13@users.noreply.github.com>
2026-01-12 13:42:50 +0900

    feat(chapter4.2.4): add string concatenation support

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -223,6 +223,10 @@ if (10 > 1) {
 			"unknown operator: BOOLEAN + BOOLEAN",
 		},
 		{
+			`"Hello" - "World!`,
+			"unknown operator: STRING - STRING",
+		},
+		{
 			"foobar",
 			"identifier not found: foobar",
 		},
@@ -314,6 +318,20 @@ addTwo(2)`
 
 func TestStringLiteral(t *testing.T) {
 	input := `"Hello World!"`
+
+	evaluated := testEval(input)
+	str, ok := evaluated.(*object.String)
+	if !ok {
+		t.Fatalf("object is not String. got=%T (%+v)", evaluated, evaluated)
+	}
+
+	if str.Value != "Hello World!" {
+		t.Errorf("String has wrong value. got=%q", str.Value)
+	}
+}
+
+func TestStringConcatenation(t *testing.T) {
+	input := `"Hello" + " " + "World!"`
 
 	evaluated := testEval(input)
 	str, ok := evaluated.(*object.String)
